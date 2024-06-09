0af45ece3e1b3a0843b80e070d98ec43182bac92
Tove-Larsson <144339361+Tove-Larsson@users.noreply.github.com>
2024-06-09 14:17:56 +0200

    Created project

---
+++ src/main/java/org/tove/group3itinformationsecurity/Group3ItInformationSecurityApplication.java
@@ -0,0 +1,13 @@
+package org.tove.group3itinformationsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Group3ItInformationSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Group3ItInformationSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/tove/group3itinformationsecurity/Group3ItInformationSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tove.group3itinformationsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Group3ItInformationSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
