47670b1d18695227822a41974be562cf2f1ce4bf
Tove-Larsson <144339361+Tove-Larsson@users.noreply.github.com>
2024-06-10 14:46:40 +0200

    Add securityFilterChain

--- src/main/java/org/tove/group3itinformationsecurity/web/Config.java
+++ src/main/java/org/tove/group3itinformationsecurity/web/Config.java
@@ -1,10 +1,31 @@
 package org.tove.group3itinformationsecurity.web;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
-
+@EnableWebSecurity
 @Configuration
 public class Config {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth ->
+                        auth
+                                .anyRequest().authenticated()
+                )
+                .formLogin(formlogin ->
+                        formlogin
+                                .defaultSuccessUrl("/", true)
+                                .failureUrl("/login?error=true")
+                                .permitAll());
+
+        return http.build();
+
+    }
 
 
 }
