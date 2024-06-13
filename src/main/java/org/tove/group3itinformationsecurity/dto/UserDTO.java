279a38571e8635fc31c481ecbdec7f664bb57e76
aldinalj <aldinalj@gmail.com>
2024-06-13 17:01:58 +0200

    Add attributes to userDTO

--- src/main/java/org/tove/group3itinformationsecurity/dto/UserDTO.java
+++ src/main/java/org/tove/group3itinformationsecurity/dto/UserDTO.java
@@ -6,12 +6,26 @@ import jakarta.validation.constraints.Size;
 
 public class UserDTO {
 
+    @NotBlank(message = "First name cannot be blank")
+    private String firstName;
+
+    @NotBlank(message = "Last name cannot be blank")
+    private String lastName;
+
+    @NotBlank(message = "Age cannot be blank")
+    private int age;
+
     @Email(message = "Please provide a valid email address")
     @NotBlank(message = "Email cannot be blank")
     private String email;
+
     @NotBlank(message = "Password cannot be blank")
     @Size(min = 3, max = 30, message = "Password must be between 3 and 30 characters")
     private String password;
+
+    @NotBlank(message = "Role cannot be blank")
+    private String role;
+
 
     public String getEmail() {
         return email;
@@ -29,11 +43,47 @@ public class UserDTO {
         this.password = password;
     }
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
     @Override
     public String toString() {
         return "UserDTO{" +
-                "email='" + email + '\'' +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                ", email='" + email + '\'' +
                 ", password='" + password + '\'' +
+                ", role='" + role + '\'' +
                 '}';
     }
 }
