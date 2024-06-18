d7164c0b50305c8889a13c21a8c58f667ade0f20
aldinalj <aldinalj@gmail.com>
2024-06-18 11:32:41 +0200

    Create initial database post

--- src/main/java/org/tove/group3itinformationsecurity/service/UserService.java
+++ src/main/java/org/tove/group3itinformationsecurity/service/UserService.java
@@ -1,27 +1,39 @@
 package org.tove.group3itinformationsecurity.service;
 
 import jakarta.annotation.PostConstruct;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.tove.group3itinformationsecurity.model.AppUser;
 import org.tove.group3itinformationsecurity.repository.UserRepository;
 
 @Service
 public class UserService {
 
     UserRepository userRepository;
+    PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         super();
         this.userRepository = userRepository;
-
+        this.passwordEncoder = passwordEncoder;
     }
 
     @PostConstruct
-    public void createAdmin() {
-        // TODO - Skapa admin & user
+    public void initialDatabasePost() {
+
+        AppUser admin = new AppUser();
+        admin.setEmail("admin");
+        admin.setPassword(passwordEncoder.encode("password"));
+        admin.setRole("ADMIN");
+        userRepository.save(admin);
+
+        AppUser user = new AppUser();
+        user.setFirstName("Clark");
+        user.setLastName("Kent");
+        user.setAge(36);
+        user.setEmail("clark.kent@smallvilehigh.com");
+        user.setPassword(passwordEncoder.encode("lanalang"));
+        user.setRole("USER");
+        userRepository.save(user);
     }
-
-
 }
